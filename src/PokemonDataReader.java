52fbe92a44c0f123d936894b5cb2f776469826ba
javilima01 <javierbilbao75@gmail.com>
2023-11-18 15:03:03 +0100

    [JaBi] fix: fixed pokemons.json

--- src/PokemonDataReader.java
+++ src/PokemonDataReader.java
@@ -1,29 +1,33 @@
-import org.json.*;
-
+import org.json.JSONArray;
+import org.json.JSONObject;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.text.html.parser.Element;
 
 public class PokemonDataReader {
     public List<Pokemon> readPokemonsFromJson(String filePath) {
-        try (FileReader reader = new FileReader(filePath)) {
-            Gson gson = new Gson();
-            Type pokemonListType = new TypeToken<List<Pokemon>>(){}.getType();
-            return gson.fromJson(reader, pokemonListType);
+
+        try {
+            String jsonData = new String(Files.readAllBytes(Paths.get(filePath)));
+
+            JSONArray pokemons = new JSONArray(jsonData);
+            System.err.println(jsonData);
+            List<Pokemon> pokemonList = new ArrayList<>();
+
+            // Now you can use the 'pokemonList' for further processing
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
         }
+        List<Pokemon> meow = new ArrayList<Pokemon>();
+        return meow;
     }
     public List<Move> readMovementsFromJson(String filePath) {
-        try (FileReader reader = new FileReader(filePath)) {
-            Gson gson = new Gson();
-            Type pokemonListType = new TypeToken<List<Pokemon>>(){}.getType();
-            return gson.fromJson(reader, pokemonListType);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+        return new ArrayList<Move>();
     }
 }
